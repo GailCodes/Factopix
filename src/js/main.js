9aa6cb76d3deda0c4ef69985733d6abf0629979b
JamieBurridge <xross_dev@proton.me>
2023-05-21 23:04:33 +0100

    add rem font size and prettier code formatting

--- src/js/main.js
+++ src/js/main.js
@@ -1,42 +1,46 @@
-const buttonEl = document.querySelector("button")
-const factTextEl = document.querySelector("p")
-const imageEl = document.querySelector("img")
-const imageLoadingEl = document.getElementById("image-loading")
+const buttonEl = document.querySelector("button");
+const factTextEl = document.querySelector("p");
+const imageEl = document.querySelector("img");
+const imageLoadingEl = document.getElementById("image-loading");
 
 async function main() {
-	buttonEl.onclick = async () => { await updateFactAndImage() }
-	await updateFactAndImage()
+  buttonEl.onclick = async () => {
+    await updateFactAndImage();
+  };
+  await updateFactAndImage();
 }
 
 async function updateFactAndImage() {
-	showLoadingStates(true)
-	factTextEl.textContent = await getRandomFact()
-	imageEl.src = await getRandomImage()
-	showLoadingStates(false)
+  showLoadingStates(true);
+  factTextEl.textContent = await getRandomFact();
+  imageEl.src = await getRandomImage();
+  showLoadingStates(false);
 }
 
 async function showLoadingStates(loading) {
-	if (loading) {
-		buttonEl.textContent = "Loading..."
-		imageEl.classList.add("hidden")
-		imageLoadingEl.classList.remove("hidden")
-	} else {
-		imageLoadingEl.classList.add("hidden")
-		imageEl.classList.remove("hidden")
-		buttonEl.textContent = "Genarete"
-	}
+  if (loading) {
+    buttonEl.textContent = "Loading...";
+    imageEl.classList.add("hidden");
+    imageLoadingEl.classList.remove("hidden");
+  } else {
+    imageLoadingEl.classList.add("hidden");
+    imageEl.classList.remove("hidden");
+    buttonEl.textContent = "Genarete";
+  }
 }
 
 async function getRandomImage() {
-	const res = await fetch("https://picsum.photos/300/300")
-	return res.url
+  const res = await fetch("https://picsum.photos/300/300");
+  return res.url;
 }
 
 async function getRandomFact() {
-	const res = await fetch("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en")
-	const resJson = await res.json()
+  const res = await fetch(
+    "https://uselessfacts.jsph.pl/api/v2/facts/random?language=en"
+  );
+  const resJson = await res.json();
 
-	return resJson.text
+  return resJson.text;
 }
 
-main()
+main();
